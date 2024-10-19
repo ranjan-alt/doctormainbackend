aacac8a4f7b0ff596ef48f22ebbfa9ed84fb765f
ranjan-alt <ranjankumarsah142@gmail.com>
2024-10-19 23:54:58 +0530

    main

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
-import connectDB from "../config/mongodb.js";
-import connectCloudinary from "../config/cloudinary.js";
-import userRouter from "../routes/userRoute.js";
-import doctorRouter from "../routes/doctorRoute.js";
-import adminRouter from "../routes/adminRoute.js";
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
+import doctorRouter from "./routes/doctorRoute.js";
+import adminRouter from "./routes/adminRoute.js";
 
 // app config
 const app = express();
