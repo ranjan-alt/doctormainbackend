c79f37c7c287359cc7abcd54c3a6f32b217e64f5
ranjan-alt <ranjankumarsah142@gmail.com>
2024-10-19 23:36:43 +0530

    index change

--- index.js
+++ index.js
@@ -7,17 +7,18 @@ import userRouter from "./routes/userRoute.js";
 import doctorRouter from "./routes/doctorRoute.js";
 import adminRouter from "./routes/adminRoute.js";
 
-// app config
+// App config
 const app = express();
-const port = process.env.PORT || 3000;
+
+// Connect to databases
 connectDB();
 connectCloudinary();
 
-// middlewares
+// Middleware
 app.use(express.json());
 app.use(cors());
 
-// api endpoints
+// API endpoints
 app.use("/api/user", userRouter);
 app.use("/api/admin", adminRouter);
 app.use("/api/doctor", doctorRouter);
@@ -26,4 +27,5 @@ app.get("/", (req, res) => {
   res.send("API Working");
 });
 
-app.listen(port, () => console.log(`Server started on PORT:${port}`));
+// Export the app for Vercel
+export default app;
