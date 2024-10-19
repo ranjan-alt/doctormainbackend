77781299c1caea2dbfd76025894d9402ef121d37
ranjan-alt <ranjankumarsah142@gmail.com>
2024-10-19 23:38:43 +0530

    index

--- index.js
+++ index.js
@@ -1,31 +1,9 @@
 import express from "express";
-import cors from "cors";
-import "dotenv/config";
-import connectDB from "./config/mongodb.js";
-import connectCloudinary from "./config/cloudinary.js";
-import userRouter from "./routes/userRoute.js";
-import doctorRouter from "./routes/doctorRoute.js";
-import adminRouter from "./routes/adminRoute.js";
 
-// App config
 const app = express();
 
-// Connect to databases
-connectDB();
-connectCloudinary();
-
-// Middleware
-app.use(express.json());
-app.use(cors());
-
-// API endpoints
-app.use("/api/user", userRouter);
-app.use("/api/admin", adminRouter);
-app.use("/api/doctor", doctorRouter);
-
 app.get("/", (req, res) => {
-  res.send("API Working");
+  res.send("Hello, world!");
 });
 
-// Export the app for Vercel
 export default app;
